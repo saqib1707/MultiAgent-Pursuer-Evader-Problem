faaeaa59544865842ab6ef51702873c2a56ffb16
Saqib Azim <azimsaqib10@gmail.com>
2018-10-22 18:53:17 +0530

    dumb silly mistake removed. worst engineering mistake one can ever do

--- code/shortest_pursuer_path_optimizer.py
+++ code/shortest_pursuer_path_optimizer.py
@@ -1,59 +1,106 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 import nlopt
+import math
 import pdb
 
-time_interval = 1.0
-number_intervals = 100
+time_interval = 0.1
 evader_max_velocity = 1.0
+pursuer_vel_lower = 0.1
 k2 = 1.0
-number_evaders = 10
-pursuer_position = np.random.uniform(low=1.0, high=5.0, size=(number_intervals+1, 2))
-evader_position = np.random.uniform(low=-1.0, high=1.0, size=(number_intervals+1, number_evaders, 2))
-centroid = np.zeros((number_intervals+1, 2), dtype=np.float32)
-epsilon = 0.1
-N = (number_intervals+1)*2            # dimension of the optimization problem
+number_evaders = Ne = 3
+number_intervals = Ni = 5
+epsilon = 0.01
+tolerance = 1e-8
+max_eval = 2000
+N = (Ne+1)*2*(Ni+1)    # dimension of the optimization problem
 
-def objective_function(time_step, grad):
-	if len(grad) > 0:
-		print('Yo')
+def objective_function(parameter_list, grad):
 	path_length = 0
-	for i in range(number_intervals+1):
-		path_length += np.sum(np.square(pursuer_position[i,:]-pursuer_position[i-1,:]))
+	for i in range(1, Ni+1):
+		index1 = (Ne*2 + 2)*i + (Ne*2)
+		index2 = (Ne*2 + 2)*(i-1) + (Ne*2)
+		path_length += np.sum(np.square((parameter_list[index1:index1+2]-parameter_list[index2:index2+2])/time_interval))
 	path_length = path_length*time_interval
 	return path_length
 
-def inequality_constraint(i, grad):   # i represents the evader number
-	if len(grad) > 0:
-		print('Yo')
-	return np.square(np.linalg.norm(evader_position[number_intervals,i,:] - centroid[number_intervals,:])) - epsilon
+def inequality_constraint_1(parameter_list, i):   # i represents the evader numbers
+	centroid = np.zeros(2)
+	for j in range(Ne):
+		index1 = Ni*(Ne*2 + 2) + 2*j
+		centroid = centroid + parameter_list[index1:index1+2]
+	centroid = centroid/Ne
+	index1 = Ni*(Ne*2 + 2) + 2*i
+	return np.square(np.linalg.norm(parameter_list[index1:index1+2] - centroid)) - epsilon
 
-def equality_constraint(i, time_step, grad):    # i represents the evader number
-	term1 = np.exp(-k2*np.linalg.norm(evader_position[time_step,i,:] - pursuer_position[time_step,:]))
-	term2 = pursuer_position[time_step,:] - pursuer_position[time_step-1,:]
-	term3 = evader_position[time_step,i,:] - pursuer_position[time_step,:]
-	# pdb.set_trace()
-	term4 = 1 + (np.matmul(term2.T, term3)/(np.linalg.norm(term2)*np.linalg.norm(term3)))
-	term5 = term3/np.linalg.norm(term3)
-	term6 = evader_position[time_step,i,:] - evader_position[time_step-1,i,:]
-	return (evader_max_velocity/2)*term1*term4*term5 - term6
+def inequality_constraint_2(parameter_list, t):
+	index1 = t*(Ne*2 + 2) + (Ne*2)
+	index2 = (t-1)*(Ne*2 + 2) + (Ne*2)
+	return pursuer_vel_lower - np.linalg.norm((parameter_list[index1:index1+2] - parameter_list[index2:index2+2])/time_interval)
 
-def main():
-	global pursuer_position
-	opt = nlopt.opt(nlopt.LN_COBYLA, N)
-	print(opt.get_algorithm_name())
-	# opt.set_lower_bounds([])
-	opt.set_min_objective(objective_function)
-	for i in range(number_evaders):
-		opt.add_inequality_constraint(lambda pursuer_position, grad:inequality_constraint(i, grad), 1e-8)   # 1e-8 is tolerance value
+def equality_constraint(parameter_list, i, t, index):    # i represents the evader number
+	index0 = (t-1)*(Ne*2 + 2) + 2*i
+	index1 = t*(Ne*2 + 2) + 2*i
+	index2 = t*(Ne*2 + 2) + (Ne*2)
+	index3 = (t-1)*(Ne*2 + 2) + (Ne*2)
 
-	# for i in range(number_evaders):
-	# 	for t in range(1,number_intervals):
-	# 		opt.add_equality_constraint(lambda pursuer_position, grad:equality_constraint(i, t, grad), 0)
+	term0 = parameter_list[index1:index1+2] - parameter_list[index2:index2+2]
+	term1 = np.exp(-k2*np.linalg.norm(term0))
+	term2 = (parameter_list[index2:index2+2] - parameter_list[index3:index3+2])/time_interval
+	term4 = 1 + (np.matmul(term2.T, term0)/(np.linalg.norm(term2)*np.linalg.norm(term0)))
+	term5 = term0/np.linalg.norm(term0)
+	term6 = (parameter_list[index1:index1+2] - parameter_list[index0:index0+2])/time_interval
+	return (evader_max_velocity/2)*term1*term4*term5[index] - term6[index]
 
-	starting_point = np.random.uniform(low=1.0, high=5.0, size=(number_intervals+1, 2))
-	optimum_values = opt.optimize(starting_point)
-	optimum_path_length = opt.last_optimum_value()
+opt = nlopt.opt(nlopt.LN_COBYLA, N)
+opt.set_min_objective(objective_function)
 
-if __name__ == '__main__':
-	main()
+parameter_lower_bound = -np.ones(N)
+for i in range(N):
+	index1 = Ne*2 + 2
+	if(i%index1 == index1-1 or i%index1 == index1-2):
+		parameter_lower_bound[i] = -float("inf")
+
+parameter_upper_bound = -parameter_lower_bound
+opt.set_lower_bounds(parameter_lower_bound)
+opt.set_upper_bounds(parameter_upper_bound)
+
+for i in range(Ne):
+	opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_1(parameter_list, i), tolerance)
+	for t in range(1, Ni+1):
+		for index in range(2):
+			opt.add_equality_constraint(lambda parameter_list, grad:equality_constraint(parameter_list, i, t, index), 0)
+
+for t in range(1, Ni+1):
+	opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_2(parameter_list, t), tolerance)
+
+opt.set_maxeval(max_eval)
+opt.set_xtol_rel(tolerance)
+
+starting_point = np.random.uniform(low=-1.0, high=1.0, size=N)
+optimal_parameters = opt.optimize(starting_point)
+optimum_path_length = opt.last_optimum_value()
+
+pursuer_position = []
+for i in range(Ni+1):
+	index = i*(Ne*2+2) + Ne*2
+	pursuer_position.append(optimal_parameters[index:index+2])
+
+all_evader_position = []
+for i in range(Ne):
+	evader_position = []
+	for t in range(Ni+1):
+		evader_position.append(optimal_parameters[t*(Ne*2 + 2) + 2*i : t*(Ne*2+2) + 2*i + 2])
+	all_evader_position.append(np.array(evader_position))
+
+all_evader_position = np.array(all_evader_position)
+pursuer_position = np.array(pursuer_position)
+
+plt.figure(1)
+for i in range(Ne):
+	plt.plot(all_evader_position[i,0,0], all_evader_position[i,0,1], marker='.', color='r')
+	plt.plot(all_evader_position[i,Ni,0], all_evader_position[i,Ni,1], marker='.', color='g')
+plt.plot(pursuer_position[:,0], pursuer_position[:,1], marker='.', color='b')
+plt.grid(True);plt.xlabel("x");plt.ylabel("y")
+plt.show()
+print("Done")
--- code/shortest_time_optimizer.py
+++ code/shortest_time_optimizer.py
@@ -3,52 +3,84 @@ import matplotlib.pyplot as plt
 import nlopt
 import pdb
 
-number_interval = 10
+number_interval = 20
+time_interval = 0.1
 v = 1.0
 epsilon = 0.001
-N = 3*(number_interval+1)
+max_eval = 200000
+tolerance = 1e-8
+N = 3*number_interval+1
+count_eval = 0
+initial_point = np.array([1.0, 1.0, np.pi/2], dtype=np.float32)
 
-def calc_time_interval(parameter_list, time_step):
-	return (np.linalg.norm(parameter_list[3*time_step:3*time_step+2] - parameter_list[3*(time_step-1):3*(time_step-1)+2]))/v
+# def objective_function(parameter_list, grad):
+# 	path_length = 0
+# 	for i in range(1, number_interval+1):
+# 		path_length += np.linalg.norm(parameter_list[3*i:3*i+2] - parameter_list[3*(i-1):3*(i-1)+2])
+# 	return path_length
 
 def objective_function(parameter_list, grad):
-	total_time = 0
-	for i in range(1, number_interval+1):
-		total_time += (np.linalg.norm(parameter_list[3*i:3*i+2] - parameter_list[3*(i-1):3*(i-1)+2]))/v
+	global count_eval
+	count_eval += 1
+	total_time = number_interval*parameter_list[3*number_interval]
 	return total_time
 
 def inequality_constraint_1(parameter_list):
-	return np.square(parameter_list[3*number_interval]) + np.square(parameter_list[3*number_interval+1]) - epsilon
+	return np.square(parameter_list[3*(number_interval-1)]) + np.square(parameter_list[3*(number_interval-1)+1]) - epsilon
 
 def inequality_constraint_2(parameter_list, time_step):
-	time_interval = calc_time_interval(parameter_list, time_step)
-	return (parameter_list[3*time_step+2] - parameter_list[3*(time_step-1)+2])/time_interval - 1
+	if time_step == 1:
+		return abs((parameter_list[3*time_step-1] - initial_point[2])/parameter_list[3*number_interval]) - 1
+	else:
+		return abs((parameter_list[3*time_step-1] - parameter_list[3*(time_step-1)-1])/parameter_list[3*number_interval]) - 1
 
 def equality_constraint_1(parameter_list, time_step):
-	time_interval = calc_time_interval(parameter_list, time_step)
-	return (parameter_list[3*time_step] - parameter_list[3*(time_step-1)])/time_interval - v*np.cos(parameter_list[3*time_step+2])
+	if time_step == 1:
+		return (parameter_list[3*(time_step-1)] - initial_point[0])/parameter_list[3*number_interval] - v*np.cos(parameter_list[3*time_step-1])
+	else:
+		return (parameter_list[3*(time_step-1)] - parameter_list[3*(time_step-2)])/parameter_list[3*number_interval] - v*np.cos(parameter_list[3*time_step-1])
 
 def equality_constraint_2(parameter_list, time_step):
-	time_interval = calc_time_interval(parameter_list, time_step)
-	return (parameter_list[3*time_step+1] - parameter_list[3*(time_step-1)+1])/time_interval - v*np.sin(parameter_list[3*time_step+2])
+	if time_step == 1:
+		return (parameter_list[3*(time_step-1)+1] - initial_point[1])/parameter_list[3*number_interval] - v*np.sin(parameter_list[3*time_step-1])
+	else:
+		return (parameter_list[3*(time_step-1)+1] - parameter_list[3*(time_step-2)+1])/parameter_list[3*number_interval] - v*np.sin(parameter_list[3*time_step-1])
 
 opt = nlopt.opt(nlopt.LN_COBYLA, N)
 opt.set_min_objective(objective_function)
-opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_1(parameter_list), 1e-8)
+
+# upper and lower bounds on the parameters
+parameter_lower_bound = -np.ones(N)
+parameter_upper_bound = np.ones(N)
+for i in range(N):
+	index1 = 3
+	if(i%index1 == index1-1):
+		parameter_lower_bound[i] = 0
+		parameter_upper_bound[i] = 2*np.pi
+
+# upper and lower bounds on the time-duration parameter
+parameter_lower_bound[3*number_interval] = 1e-8
+parameter_upper_bound[3*number_interval] = 100
+
+opt.set_lower_bounds(parameter_lower_bound)
+opt.set_upper_bounds(parameter_upper_bound)
+
+opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_1(parameter_list), tolerance)
 for time_step in range(1, number_interval+1):
-	opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_2(parameter_list, time_step), 1e-8)
+	opt.add_inequality_constraint(lambda parameter_list, grad:inequality_constraint_2(parameter_list, time_step), tolerance)
 	opt.add_equality_constraint(lambda parameter_list, grad:equality_constraint_1(parameter_list, time_step), 0)
 	opt.add_equality_constraint(lambda parameter_list, grad:equality_constraint_2(parameter_list, time_step), 0)
 
-opt.set_xtol_rel(1e-8)
-starting_point = np.random.uniform(low=0, high=5.0, size=3*(number_interval+1))
-opt_parameter = opt.optimize(starting_point)
-print("Final Optimized Time to reach near origin : ", opt.last_optimum_value())
-# print("Final Optimized Time to reach near origin (manually) : ", objective_function(opt_parameter, []))
+opt.set_maxeval(max_eval)
+opt.set_xtol_rel(tolerance)
 
-x_coord = []
-y_coord = []
-for i in range(number_interval+1):
+starting_point = np.random.uniform(low=0, high=1.0, size=N)
+opt_parameter = opt.optimize(starting_point)
+print("Final Optimized time to reach near origin : ", opt.last_optimum_value())
+
+x_coord = [initial_point[0]]
+y_coord = [initial_point[1]]
+for i in range(number_interval):
 	x_coord.append(opt_parameter[3*i])
 	y_coord.append(opt_parameter[3*i+1])
 
@@ -56,6 +88,9 @@ x_coord = np.array(x_coord)
 y_coord = np.array(y_coord)
 
 plt.figure(1)
-plt.plot(x_coord, y_coord);plt.grid(True);plt.xlabel("x");plt.ylabel("y")
+plt.plot(x_coord, y_coord, marker='.');plt.grid(True);plt.xlabel("x");plt.ylabel("y")
+circle = plt.Circle((0, 0), np.sqrt(epsilon), color='r', fill=False)
+ax = plt.gca()
+ax.add_artist(circle)
 plt.show()
-print("Done")
+print(count_eval, "Done")
