3a5f07376d6b08cb4f7c1ce3de983fc878aa0242
saqib1707 <sach.goyalsachin@gmail.com>
2019-03-22 02:14:19 +0530

    adding potential patterns/features

--- code/pursuer_evader_aggregation/greedy_approach.m
+++ code/pursuer_evader_aggregation/greedy_approach.m
@@ -1,7 +1,7 @@
 clear; clc;
 
 % ---------------------hyper-parameters------------------------
-hp.number_interval = 28;
+hp.number_interval = 50;
 hp.time_interval = 1.0;
 hp.number_evader = 2;
 hp.number_pursuer = 1;
@@ -9,10 +9,9 @@ hp.vemax_repulsion = 0.4;
 hp.vemax_attraction = 0;
 hp.vp = 0.1;
 hp.K = 1.0;
-hp.epsilon = 0.05;
-
-hp.var = 2*hp.number_pursuer;
-hp.N = hp.var*hp.number_interval;
+hp.epsilon = 0.05*2;
+hp.angle_resolution = 1.0;
+hp.alpha = 0.8;
 
 % file = load('data_file.mat');
 
@@ -21,41 +20,51 @@ hp.initial_pursuer_position = [-1;-1];
 
 % hp.initial_evader_position = rand(2*hp.number_evader,1)*2-1;
 % hp.initial_evader_position = file.initial_evader_position;
-hp.initial_evader_position = [0;0;1;1];
+hp.initial_evader_position = [0.5;0;-0.5;0];
 
-hp.destination = [2;2];
+hp.destination = [0;0];
 % ---------------------------hyper-parameters------------------------
 
 pursuer_trajectory = zeros(2*hp.number_pursuer,hp.number_interval+1);
 evader_trajectory = zeros(2*hp.number_evader,hp.number_interval+1);
 pursuer_trajectory(:,1) = hp.initial_pursuer_position;
 evader_trajectory(:,1) = hp.initial_evader_position;
-distance_list = zeros(360,1);
+distance_list = zeros(4,hp.number_interval+1);
 
-% while sum(sqrt(sum((reshape(evader_trajectory(:,t),2,hp.number_evader) - repmat(hp.destination,[1,hp.number_evader])).^2,1)) > hp.epsilon) ~= 0
 for t = 1:hp.number_interval
-	for angle = 1:1:360
+    metric_list = zeros(360/hp.angle_resolution,1);
+	for angle = 0:hp.angle_resolution:359.999
 		pursuer_evader_vector = reshape(evader_trajectory(:,t),2,hp.number_evader) - repmat(pursuer_trajectory(:,t),[1,hp.number_evader]);
 		pursuer_evader_distance = sqrt(sum(pursuer_evader_vector.^2,1));
 		pursuer_temp_position = pursuer_trajectory(:,t) + [hp.vp*cos(angle);hp.vp*sin(angle)]*hp.time_interval;
-		pursuer_velocity = (pursuer_temp_position - pursuer_trajectory(:,t))/hp.time_interval;
+		pursuer_velocity = [hp.vp*cos(angle);hp.vp*sin(angle)];
 		costheta = (transpose(pursuer_velocity)*pursuer_evader_vector)./(pursuer_evader_distance*norm(pursuer_velocity));
 		repulsion_term = reshape(0.5*hp.vemax_repulsion*repmat(exp(-hp.K*pursuer_evader_distance).*(1+costheta),[2,1]).*(pursuer_evader_vector./repmat(pursuer_evader_distance,[2,1])),2*hp.number_evader,1);
 		evader_temp_position = evader_trajectory(:,t) + repulsion_term*hp.time_interval;
 		evader_destination_vector = reshape(evader_temp_position,2,hp.number_evader) - repmat(hp.destination,[1,hp.number_evader]);
 		evader_destination_total_distance = sum(sqrt(sum(evader_destination_vector.^2,1)));
-		distance_list(angle,1) = evader_destination_total_distance;
+        inter_evader_distance = norm(evader_temp_position(1:2,1) - evader_temp_position(3:4,1));
+		metric_list(angle/hp.angle_resolution+1,1) = hp.alpha*evader_destination_total_distance + (1-hp.alpha)*inter_evader_distance;
 	end
-	[~,loc] = min(distance_list);
-	pursuer_trajectory(:,t+1) = pursuer_trajectory(:,t) + [hp.vp*cos(loc);hp.vp*sin(loc)]*hp.time_interval;
+	[~,loc] = min(metric_list);
+    optimal_angle = (loc-1)*hp.angle_resolution;
+	pursuer_trajectory(:,t+1) = pursuer_trajectory(:,t) + [hp.vp*cos(optimal_angle);hp.vp*sin(optimal_angle)]*hp.time_interval;
 	pursuer_evader_vector = reshape(evader_trajectory(:,t),2,hp.number_evader) - repmat(pursuer_trajectory(:,t),[1,hp.number_evader]);
 	pursuer_evader_distance = sqrt(sum(pursuer_evader_vector.^2,1));
 	pursuer_velocity = (pursuer_trajectory(:,t+1) - pursuer_trajectory(:,t))/hp.time_interval;
 	costheta = (transpose(pursuer_velocity)*pursuer_evader_vector)./(pursuer_evader_distance*norm(pursuer_velocity));
 	repulsion_term = reshape(0.5*hp.vemax_repulsion*repmat(exp(-hp.K*pursuer_evader_distance).*(1+costheta),[2,1]).*(pursuer_evader_vector./repmat(pursuer_evader_distance,[2,1])),2*hp.number_evader,1);
 	evader_trajectory(:,t+1) = evader_trajectory(:,t) + repulsion_term*hp.time_interval;
+    evader_dest_distance = sqrt(sum((reshape(evader_trajectory(:,t),2,hp.number_evader) - repmat(hp.destination,[1,hp.number_evader])).^2,1));
+    inter_evader_distance = norm(evader_trajectory(1:2,t) - evader_trajectory(3:4,t));
+    distance_list(:,t) = [transpose(evader_dest_distance);inter_evader_distance;hp.alpha*sum(evader_dest_distance)+(1-hp.alpha)*inter_evader_distance];
+%     if t == 100
+%         hp.alpha = 1.0;
+%     end
 end
-
+evader_dest_distance = sqrt(sum((reshape(evader_trajectory(:,hp.number_interval+1),2,hp.number_evader) - repmat(hp.destination,[1,hp.number_evader])).^2,1));
+inter_evader_distance = norm(evader_trajectory(1:2,hp.number_interval+1) - evader_trajectory(3:4,hp.number_interval+1));
+distance_list(:,hp.number_interval+1) = [transpose(evader_dest_distance);inter_evader_distance;hp.alpha*sum(evader_dest_distance)+(1-hp.alpha)*inter_evader_distance];
 
 figure;
 plot(pursuer_trajectory(1,:), pursuer_trajectory(2,:), 'o-', 'color', 'blue');hold on;
@@ -64,6 +73,7 @@ for i=1:hp.number_evader
     plot(evader_trajectory(2*i-1,2:hp.number_interval),evader_trajectory(2*i,2:hp.number_interval),'o-','color','yellow');hold on;
     plot(evader_trajectory(2*i-1,hp.number_interval+1),evader_trajectory(2*i,hp.number_interval+1), 'o-', 'color', 'red');hold on;
 end
+
 for t = 1:hp.number_interval
    plot([evader_trajectory(1,t),evader_trajectory(3,t)],[evader_trajectory(2,t),evader_trajectory(4,t)],'r');
 end
@@ -72,5 +82,15 @@ draw_circle(hp.destination(1,1), hp.destination(2,1), hp.epsilon);
 grid on;
 xlabel('X');
 ylabel('Y');
-title('shepherding-optimization-result');
+title('shepherding-greedy-approach');
+hold off;
+
+figure;
+plot(distance_list(1,:),'color','red');hold on;
+plot(distance_list(2,:),'color','green');hold on;
+plot(distance_list(3,:),'color','blue');hold on;
+plot(distance_list(4,:),'color','black');hold on;
+grid on;
+ylabel('distance-metric');
+xlabel('time-step');
 hold off;
--- code/pursuer_evader_aggregation/heuristic_features.m
+++ code/pursuer_evader_aggregation/heuristic_features.m
@@ -1,47 +1,81 @@
 clear;clc;
-load('../../results-plots/15-03-19/hyperparameters/8.mat');
-pursuer_trajectory = horzcat(hp.initial_pursuer_position,reshape(hp.opt_x,2,hp.number_interval));
-evader_trajectory = compute_evader_position(pursuer_trajectory,hp.number_evader,hp.initial_evader_position,hp.number_interval,hp.time_interval,hp.vemax_repulsion,hp.vemax_attraction,hp.K);
-evader_center_trajectory = (evader_trajectory(1:2,:)+evader_trajectory(3:4,:))/2;
-figure;
-plot(pursuer_trajectory(1,:), pursuer_trajectory(2,:), 'o-', 'color', 'blue');hold on;
-for i=1:hp.number_evader
-    plot(evader_trajectory(2*i-1,1),evader_trajectory(2*i,1), 'o-', 'color', 'green');hold on;
-    plot(evader_trajectory(2*i-1,2:hp.number_interval),evader_trajectory(2*i,2:hp.number_interval),'o-','color','yellow');hold on;
-    plot(evader_trajectory(2*i-1,hp.number_interval+1),evader_trajectory(2*i,hp.number_interval+1), 'o-', 'color', 'red');hold on;
-end
-for t = 1:hp.number_interval
-    plot([evader_trajectory(1,t),evader_trajectory(3,t)],[evader_trajectory(2,t),evader_trajectory(4,t)],'r');
-    plot([evader_center_trajectory(1,t),pursuer_trajectory(1,t)],[evader_center_trajectory(2,t),pursuer_trajectory(2,t)],'black');
-    plot([evader_center_trajectory(1,t),hp.destination(1,1)],[evader_center_trajectory(2,t),hp.destination(2,1)],'red');
-end
-draw_circle(hp.destination(1,1), hp.destination(2,1), hp.epsilon);
-grid on;
-xlabel('X');
-ylabel('Y');
-title('shepherding-optimization-result');
-hold off;
+for index=1:1
+    filename = strcat('../../results-plots/15-03-19/hyperparameters/',strcat(int2str(index),'.mat'));
+    load(filename);
+    pursuer_trajectory = horzcat(hp.initial_pursuer_position,reshape(hp.opt_x,2,hp.number_interval));
+    evader_trajectory = compute_evader_position(pursuer_trajectory,hp.number_evader,hp.initial_evader_position,hp.number_interval,hp.time_interval,hp.vemax_repulsion,hp.vemax_attraction,hp.K);
+    evader_center_trajectory = (evader_trajectory(1:2,:)+evader_trajectory(3:4,:))/2;
 
-% theta = zeros(hp.number_interval,1);
-% for t = 1:hp.number_interval
-%     a = pursuer_trajectory(:,t) - evader_center_trajectory(:,t);
-%     b = hp.destination - evader_center_trajectory(:,t);
-%     theta(t,1) = acos(dot(a,b)/(norm(a)*norm(b)))*(180/pi);
-% end
-% 
-% figure;
-% plot(theta, 'o-', 'color', 'blue');hold on;
-% grid on;
-% xlabel('time step');
-% ylabel('angle (in degrees)');
-% title('angle between pursuer\_position and destination wrt evader\_center_trajectory');
-% hold off;
+    figure;
+    plot(pursuer_trajectory(1,:), pursuer_trajectory(2,:), 'o-', 'color', 'blue');hold on;
+    for i=1:hp.number_evader
+        plot(evader_trajectory(2*i-1,1),evader_trajectory(2*i,1), 'o-', 'color', 'green');hold on;
+        plot(evader_trajectory(2*i-1,2:hp.number_interval),evader_trajectory(2*i,2:hp.number_interval),'o-','color','yellow');hold on;
+        plot(evader_trajectory(2*i-1,hp.number_interval+1),evader_trajectory(2*i,hp.number_interval+1), 'o-', 'color', 'red');hold on;
+    end
+    for t = 1:hp.number_interval
+         plot([evader_trajectory(1,t),evader_trajectory(3,t)],[evader_trajectory(2,t),evader_trajectory(4,t)],'red');
+    %     plot([evader_center_trajectory(1,t),pursuer_trajectory(1,t)],[evader_center_trajectory(2,t),pursuer_trajectory(2,t)],'black');
+    %     plot([evader_center_trajectory(1,t),hp.destination(1,1)],[evader_center_trajectory(2,t),hp.destination(2,1)],'red');
+    %     plot([evader_trajectory(1,t),pursuer_trajectory(1,t)],[evader_trajectory(2,t),pursuer_trajectory(2,t)],'green');
+    %     plot([evader_trajectory(3,t),pursuer_trajectory(1,t)],[evader_trajectory(4,t),pursuer_trajectory(2,t)],'red');
+    end
+    draw_circle(hp.destination(1,1), hp.destination(2,1), hp.epsilon);
+    grid on;
+    xlabel('X');
+    ylabel('Y');
+    title('shepherding-optimization-result');
+    hold off;
 
-evader_separation = sqrt(sum((evader_trajectory(1:2,:) - evader_trajectory(3:4,:)).^2,1));
-figure;
-plot(evader_separation, 'o-', 'color', 'blue');hold on;
-grid on;
-xlabel('time step');
-ylabel('evader separation');
-title('separation between evaders');
-hold off;
+    % angle between the pursuer-evader_center and destination-evader_center
+    % theta = zeros(hp.number_interval,1);
+    % for t = 1:hp.number_interval
+    %     a = pursuer_trajectory(:,t) - evader_center_trajectory(:,t);
+    %     b = hp.destination - evader_center_trajectory(:,t);
+    %     theta(t,1) = acos(dot(a,b)/(norm(a)*norm(b)))*(180/pi);
+    % end
+    % 
+    % figure;
+    % plot(theta, 'o-', 'color', 'blue');hold on;
+    % grid on;
+    % xlabel('time step');
+    % ylabel('angle (in degrees)');
+    % title('angle between pursuer\_position and destination wrt evader\_center_trajectory');
+    % hold off;
+
+    % plotting the evader-separation over time 
+    evader_separation = sqrt(sum((evader_trajectory(1:2,:) - evader_trajectory(3:4,:)).^2,1));
+    h1 = figure;
+    plot(evader_separation, 'o-', 'color', 'blue');hold on;
+    grid on;
+    xlabel('time step');
+    ylabel('evader separation');
+    title('separation between evaders');
+    hold off;
+%     savefilename = strcat('../../results-plots/15-03-19/plots/',strcat(int2str(index),'_f1.fig'));
+%     savefig(h1, savefilename);
+
+    % plotting the angle of the evader-vector measured wrt X-axis
+    evader_vector_slope = (evader_trajectory(4,:) - evader_trajectory(2,:))./(evader_trajectory(3,:) - evader_trajectory(1,:));
+    evader_vector_angle = atan(evader_vector_slope)*180/pi;
+    % evader_vector_angle(evader_vector_angle < 0) = 180 + evader_vector_angle(evader_vector_angle < 0);
+    h2 = figure;
+    plot(evader_vector_angle, 'o-', 'color', 'blue');hold on;
+    grid on;
+    xlabel('time step');
+    ylabel('evader vector slope(in degrees)');
+    title('slope of line joining evaders');
+    hold off;
+%     savefilename = strcat('../../results-plots/15-03-19/plots/',strcat(int2str(index),'_f2.fig'));
+%     savefig(h2, savefilename);
+
+%     checking whether the two triangles are similar
+    pursuer_evader_vector = evader_trajectory - repmat(pursuer_trajectory,[hp.number_evader,1]);
+    evader_evader_vector = evader_trajectory(1:2,:) - evader_trajectory(3:4,:);
+    pursuer_evader_distance = [sqrt(sum(pursuer_evader_vector(1:2,:).^2,1));sqrt(sum(pursuer_evader_vector(3:4,:).^2,1))];
+    evader_evader_distance = sqrt(sum(evader_evader_vector.^2,1));
+    similarity_ratio = [pursuer_evader_distance(:,2:hp.number_interval+1)./pursuer_evader_distance(:,1:hp.number_interval);...
+        evader_evader_distance(:,2:hp.number_interval+1)./evader_evader_distance(:,1:hp.number_interval)];
+
+   close all;
+end
